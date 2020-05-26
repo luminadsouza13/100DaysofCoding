168c248200b8e9f47fb8b25afe3f777bf75ab85c
rj <reeshu13489@yahoo.com>
2020-05-26 13:31:06 -0400

    Day 5 - simple basic python condition using if else

---
+++ Day5.py
@@ -0,0 +1,42 @@
+/*
+Task
+Given an integer, , perform the following conditional actions:
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
+A single line containing a positive integer, .
+Constraints
+
+Output Format
+Print Weird if the number is weird; otherwise, print Not Weird.
+*/
+
+#!/bin/python
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+
+
+if n%2 != 0:
+    print("Weird")
+else:
+    if n>=2 and n<=5:
+        print("Not Weird")
+    elif n>=6 and n<=20:
+        print("Weird")
+    else:
+        print("Not Weird")
+
+
+
+
